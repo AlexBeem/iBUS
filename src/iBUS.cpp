c8e4b5dd69196efd3b8aed0bbbf31847cdb5f09b
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2018-11-22 09:53:02 +0100

    Added TX functionality

--- src/iBUS.cpp
+++ src/iBUS.cpp
@@ -3,20 +3,25 @@
 iBus::iBus(HardwareSerial& serial):
 m_ser(serial)
 {
-		serial.begin(115200);
-		this->m_ser = serial;
+	serial.begin(115200);
+	this->m_ser = serial;
 }
 
 iBus::iBus(SoftwareSerial& serial):
 m_ser(serial)
 {
-		serial.begin(115200);
-		this->m_ser = serial;
+	serial.begin(115200);
+	this->m_ser = serial;
 }
 
 int iBus::get_channel(int ch)
 {
 	return m_channel[ch];
+}
+
+void iBus::set_channel(int ch, int val)
+{
+	m_channel_out[ch] = val;
 }
 
 bool iBus::is_alive()
@@ -28,9 +33,10 @@ bool iBus::is_alive()
 	return false;
 }
 
-void iBus::handle()
+void iBus::handle(unsigned int timeout)
 {
-	if(m_ser.available())
+	uint32_t start = millis();
+	while(m_ser.available() && millis()-start < timeout)
 	{
 		uint8_t c = m_ser.read();
 
@@ -40,36 +46,68 @@ void iBus::handle()
 			m_packet_offset = 0;
 		}
 
-		if(m_packet_offset >= m_packet_size)
+		if(m_packet_offset >= m_packet_size-1 && m_in_packet)
 		{
-			m_packet_offset = m_packet_size;
 			m_in_packet = false;
+			m_packet[m_packet_size-1] = c;
+		
 			if(m_checksum_check(m_packet))
 			{
 				m_parse_channels(m_packet, m_channel);
 				m_last_iBus_packet = millis();
 			}
+					
 			m_packet[0] = 0x00; // Meant to signify error/no packet
 		}
-		else
+		
+		if(m_in_packet)
 		{
 			m_packet[m_packet_offset++] = c; // Save byte and iterate offset
 		}
 	}
+
+	if(is_alive() && millis() - m_last_packet_sent > m_minimum_packet_spacing)
+	{
+		m_last_packet_sent = millis();
+		m_send_packet(m_channel_out);
+	}
 }
 
-bool iBus::m_checksum_check(uint8_t packet[])
+void iBus::m_send_packet(int ch[])
 {
-	int recieved_checksum = packet[30] << 8 | packet[29];
-	int ch[14];
-	m_parse_channels(packet, ch);
+	uint8_t buff[1+ m_channels_per_packet*2 +2];
+	buff[0] = 0x55;
 
+	for(int i=0; i<m_channels_per_packet; i++)
+	{
+		buff[i*2+1] = (ch[i] & 0x00FF);
+		buff[i*2+2] = (ch[i] >> 8);
+	}
+	int checksum = m_get_checksum(ch);
+
+	buff[29] = (checksum & 0x00FF);
+	buff[30] = (checksum >> 8);
+
+	m_ser.write(buff, 1+ m_channels_per_packet*2 +2);
+}
+
+int iBus::m_get_checksum(int ch[])
+{
 	int sum = 0;
 	for(int i=0; i<m_channels_per_packet; i++)
 	{
 		sum += ch[i];
 	}
-	return recieved_checksum == sum;
+	return sum;
+}
+
+bool iBus::m_checksum_check(uint8_t packet[])
+{
+	int recieved_checksum = packet[30] << 8 | packet[29];
+	int ch[m_channels_per_packet];
+	m_parse_channels(packet, ch);
+
+	return (recieved_checksum == m_get_checksum(ch));
 }
 
 void iBus::m_parse_channels(uint8_t packet[], int ch[])
--- src/iBUS.h
+++ src/iBUS.h
@@ -11,8 +11,10 @@ public:
     iBus(SoftwareSerial& serial);
     
     int get_channel(int ch);
+    void set_channel(int ch, int val);
+
     bool is_alive();
-    void handle();
+    void handle(unsigned int timeout = 10);
 
     uint32_t time_since_last();
 
@@ -28,12 +30,21 @@ private:
     uint32_t m_last_iBus_packet = 0;
 
     uint8_t m_packet[m_packet_size];
+    uint8_t m_last_packet[m_packet_size];
+
     int m_channel[m_channels_per_packet];
+    int m_channel_out[m_channels_per_packet];
 
     bool m_in_packet = false;
     int m_packet_offset = 0;
 
+    int  m_get_checksum(int ch[]);
     bool m_checksum_check(uint8_t packet[]);
     void m_parse_channels(uint8_t packet[], int ch[]);
     void m_copy_array(uint8_t src[], uint8_t dst[], int dst_size = 0);
+
+    unsigned int m_minimum_packet_spacing = 10; // Minimum space between sent packages in ms
+    uint32_t m_last_packet_sent = 0;
+    void m_send_packet(int ch[]);
+    
 };
